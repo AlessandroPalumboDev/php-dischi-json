fea7a3f11ac9d7b5e97e2e1675e3123c718c1185
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-06-20 22:19:06 +0200

    [UPD] Aggiunto logo e dato stile

--- index.php
+++ index.php
@@ -4,15 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Dischi JSON</title>
+    <link rel="icon" href="./src/img/logo.webp" />
+    
     <!-- bootstrap -->
      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-</head>
+    <!--  my css -->
+      <link rel="stylesheet" href="./style.css">
+    </head>
 <body class="bg-black">
     <div id="app">
-        <header>
+        <header class="bg-dark mb-5">
             <div class="container">
                 <div class="row">
-                    <div class="col text-center py-3">
+                    <div class="col text-center py-1 d-flex align-items-center justify-content-around">
+                        <div id="logo">
+                            <img src="./src/img/logo.webp" alt="">
+                        </div>
                         <h1 class="text-light">
                             {{title}}
                         </h1>
@@ -23,19 +30,29 @@
 
         <main>
             <div class="container">
+
                 <div class="row align-items-center justify-content-center">
+
                     <div class="col-10 d-flex flex-wrap align-items-center justify-content-center ">
-                        <div class="card m-3 d-flex align-items-center text-center bg-dark text-light" style="width: 18rem;" v-for="album in albums">
+
+                        <div class="card m-3 d-flex align-items-center text-center bg-dark text-light" v-for="album in albums">
+
                             <img :src="album.immagine" class="card-img-top p-3" :alt="album.titolo">
-                            <div class="card-body d-flex flex-column">
+
+                            <div class="card-body d-flex flex-column gap-2 mb-3">
+
                                 <h5 class="card-title">{{album.titolo}}</h5>
                                 <span class="card-text">{{album.artista}}</span>
                                 <span class="card-text">{{album.anno}}</span>
                                 
                             </div>
+
                         </div>
+
                     </div>
+
                 </div>
+
             </div>
         </main>
     </div>
